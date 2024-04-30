10b4bf1a9e5e5f3dd4a51617973ee49b37a70558
Gustavo Keller <gustavo.lealtec@gmail.com>
2024-04-30 09:45:43 -0300

    add: testfy into project

--- internal/domain/campaign/campaign_test.go
+++ internal/domain/campaign/campaign_test.go
@@ -2,30 +2,29 @@ package campaign
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestNewCampaign(t *testing.T) {
 
+	// Three A's: Arrange, Act, Assert
+
+	// Arrange: setup variables and input
+	assert := assert.New(t)
+	id := "123"
 	name := "New Campaign"
 	content := "Body Campaign"
 	contacts := []string{"email1@campaign.com", "email2@campaign.com"}
 
+	// Act: execute the code to test
 	campaign := NewCampaign(name, content, contacts)
 
-	if campaign.ID == "" {
-		t.Error("ID must not be empty")
-	}
-	if campaign.CreateOn.IsZero() {
-		t.Error("CreateOn must not be empty")
-	}
-	if campaign.Name != name {
-		t.Error("Name must be", name)
-	}
-	if campaign.Content != content {
-		t.Error("Content must be", content)
-	}
-	if len(campaign.Contact) != len(contacts) {
-		t.Error("Contact length must be", len(contacts))
-	}
-
+	// Assert: check the output
+	assert.NotNil(campaign)
+	assert.Equal(name, campaign.Name)
+	assert.Equal(content, campaign.Content)
+	assert.Len(campaign.Contact, 2)
+	assert.Equal(contacts[0], campaign.Contact[0].Email)
+	assert.NotEqual(id, campaign.ID)
 }
